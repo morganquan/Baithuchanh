f6625a7b4cd37581334505627fb3e88a87ad9466
morganquan <phamquan2410005@gmail.com>
2024-11-29 15:14:25 +0700

    Add files via upload

---
+++ Thuc hanh 2/bai5 thuc hanh 2.py
@@ -0,0 +1,7 @@
+print("Pham Viet Quan Mssv 235752021610022")
+n = int(input("Nhập vào một số tự nhiên n > 0: "))
+if n > 0:
+    for i in range(n, -1, -1):
+        print(i)
+else:
+    print("Vui lòng nhập một số tự nhiên lớn hơn 0.")
